27a33f97dacebde2a262984e5afda6d4ffedb4ee
yangbohr <yangbo@umeng.com>
2014-02-04 12:51:01 -0800

    framework base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IQMITest::Application.config.secret_key_base = 'a2651ee8d2fdd866f212bc612991400db757848ebc1c6a056be1a00a9fd46055ce1eb08ea18a83df30109199e1e1162dd1d7d5c64d4672af211becb6df545d86'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IQMITest::Application.config.session_store :cookie_store, key: '_IQMI_test_session'
